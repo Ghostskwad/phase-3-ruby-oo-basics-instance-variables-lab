49a1508edc699523cd778f43441ba987190a7664
Quade Houston <quadehouston@gmail.com>
2022-08-30 09:06:18 -0400

    commit tree 495141980ee87624d9d6d9be624d66b7dbd967ac

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
+   
+    # def name=(dog_name)
+    #     this_dogs_name = dog_name
+    #   end
+    
+    #           #logal varibals are confined to the methods they are defined in
+
+    #   def name
+    #     this_dogs_name        #cannot read this variable since it was defined locally in def name=
+    #   end
+
+
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    def name
+        @this_dogs_name
+    end
 
 end
+
+lassie = Dog.new
+
+lassie.name= "Lassie"
+
+puts lassie.name
